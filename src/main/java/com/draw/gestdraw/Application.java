88e5bf48a71b08c7a2be0749926abfa34c11a327
Per Olin <per@rtail.se>
2023-01-10 19:08:36 +0100

    change start dimensions

--- src/main/java/com/draw/gestdraw/Application.java
+++ src/main/java/com/draw/gestdraw/Application.java
@@ -11,7 +11,7 @@ public class Application extends javafx.application.Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("views/main.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1920, 1080);
+        Scene scene = new Scene(fxmlLoader.load(), 1100, 1350);
         stage.setTitle("GestDraw");
 
         stage.setScene(scene);
